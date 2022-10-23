6edf0df40bbeeb4e5f99502c698bccffde350080
oscardv18 <oscarddiazvelasquez@gmail.com>
2022-10-23 00:59:20 -0400

    edited the route for root, new writte in the dom

--- server/routes.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "net/http"
-
-// root route handler
-func rootHandler(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("201 WELCOME"))
-}
